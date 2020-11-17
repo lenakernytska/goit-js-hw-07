6d01aa9a418e239a57d6fb54401b6550e1f2e5cd
Lena Kernytska <yaapril222@gmail.com>
2020-11-17 22:25:44 +0300

    Добавляет решение заданий 4-7

--- js/task4.js
+++ js/task4.js
@@ -1,12 +1,21 @@
-// Задание 4
-// Счетчик состоит из спана и кнопок,
-//   которые должны увеличивать и уменьшать значение счетчика на 1.
+let counterValue = 0;
 
-// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
-// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
-// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
-// <div id="counter">
-//   <button type="button" data-action="decrement">-1</button>
-//   <span id="value">0</span>
-//   <button type="button" data-action="increment">+1</button>
-// </div>
+ const decrement=()=>{
+  counterValue-=1;
+  result();
+}
+const increment=()=>{
+  counterValue+=1;
+  result();
+}
+
+const result=()=> {
+  valueRef.textContent = counterValue;
+}
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueRef = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', decrement);
+incrementBtn.addEventListener('click', increment);
